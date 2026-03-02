b73b8690f40abf547c8672739469c060450b2055
Rafhael <rafhaelaugustodev@gmail.com>
2026-03-01 23:51:39 -0300

    chore: project setup

---
+++ lib/auth/auth.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const client = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(client, { provider: "postgresql" }),
+  baseURL: process.env.BETTER_AUTH_URL,
+  emailAndPassword: { enabled: true },
+});
